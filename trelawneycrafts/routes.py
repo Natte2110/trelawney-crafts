0196b74dce8fac9dba581c151b76c697dcb43eb5
Natte2110 <natteparsley@gmail.com>
2024-01-10 11:28:12 +0000

    feat: Add functionality to add user to database

--- trelawneycrafts/routes.py
+++ trelawneycrafts/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, request, redirect, url_for, session
 from trelawneycrafts import app, db
 from trelawneycrafts.models import User, Category, Post, Reaction, Comment
 
@@ -9,3 +9,17 @@ def home():
 @app.route("/gallery")
 def gallery():
     return render_template("gallery.html", title="Gallery")
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        user = User(username=request.form.get("username"), password=request.form.get("username"), email=request.form.get("email"))
+        db.session.add(user)
+        db.session.commit()
+        return redirect(url_for("log_in"))
+    else:
+        return render_template("register.html", title="Register")
+
+@app.route("/log-in")
+def log_in():
+    return render_template("login.html", title="Log In")
