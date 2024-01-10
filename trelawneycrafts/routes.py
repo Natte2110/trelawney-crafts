fe6eb49a6e8ca4a1ce5c744fbe5387448b4eee65
Natte2110 <natteparsley@gmail.com>
2024-01-10 10:14:46 +0000

    feat: Add route for gallery page

--- trelawneycrafts/routes.py
+++ trelawneycrafts/routes.py
@@ -1,6 +1,11 @@
 from flask import render_template
 from trelawneycrafts import app, db
+from trelawneycrafts.models import User, Category, Post, Reaction, Comment
 
 @app.route("/")
 def home():
-    return render_template("base.html")
+    return render_template("index.html", title="Home")
+
+@app.route("/gallery")
+def gallery():
+    return render_template("gallery.html", title="Gallery")
