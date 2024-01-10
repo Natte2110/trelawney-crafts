412c3dcc6aa50b95cda3110c06cf74e95f7af628
Natte2110 <natteparsley@gmail.com>
2024-01-10 11:53:27 +0000

    feat: Add checks to ensure login details are unique

--- trelawneycrafts/routes.py
+++ trelawneycrafts/routes.py
@@ -13,10 +13,20 @@ def gallery():
 @app.route("/register", methods=["GET", "POST"])
 def register():
     if request.method == "POST":
-        user = User(username=request.form.get("username"), password=request.form.get("username"), email=request.form.get("email"))
-        db.session.add(user)
-        db.session.commit()
-        return redirect(url_for("log_in"))
+        usernames = User.query.with_entities(User.username).all()
+        emails = User.query.with_entities(User.email).all()
+        print(f"Current Users: {usernames[0][0]}")
+        print(f"Current Users: {emails[0][0]}")
+        if request.form.get("username") not in usernames[0]:
+            if request.form.get("email") not in emails[0]:
+                user = User(username=request.form.get("username"), password=request.form.get("username"), email=request.form.get("email"))
+                db.session.add(user)
+                db.session.commit()
+                return redirect(url_for("log_in"))
+            else:
+                return render_template("register.html", title="Register", error="email")
+        else:
+            return render_template("register.html", title="Register", error="username")
     else:
         return render_template("register.html", title="Register")
 
