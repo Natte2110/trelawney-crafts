2809e4d83d0b9cf7b5fe3c0cebdcb87ffdd10676
Natte2110 <natteparsley@gmail.com>
2024-01-31 19:45:29 +0000

    fix: Check code with validator

--- trelawneycrafts/static/js/post.js
+++ trelawneycrafts/static/js/post.js
@@ -1,6 +1,6 @@
-imageInput = document.getElementById("image");
-imagePreview = document.getElementById("image-preview");
-imageInput.onchange = (evt) => {
+let imageInput = document.getElementById("image");
+let imagePreview = document.getElementById("image-preview");
+imageInput.onchange = function () {
     const [file] = imageInput.files;
     if (file) {
         imagePreview.src = URL.createObjectURL(file);
