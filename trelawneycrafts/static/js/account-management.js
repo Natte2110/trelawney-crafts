6007d9ddb05993a7028c9e3a69fb420b1fd7eca5
Natte2110 <natteparsley@gmail.com>
2024-01-31 20:01:49 +0000

    refactor: Minor changes in accordance with validator

--- trelawneycrafts/static/js/account-management.js
+++ trelawneycrafts/static/js/account-management.js
@@ -1,27 +1,27 @@
 document.getElementById("delete-account").addEventListener("click", () => {
-    $('#delete-account-modal').modal('toggle')
+    $("#delete-account-modal").modal("toggle")
 })
 
 $("#delete-form").submit(function (e) {
     e.preventDefault();
     fetch("/delete-account", {
-        method: 'POST',
+        method: "POST",
         headers: {
-            'Content-Type': 'application/json',
+            "Content-Type": "application/json",
         },
         body: JSON.stringify({
-            password: $('#password-check').val()
+            password: $("#password-check").val()
         }),
     })
-        .then(response => response.json())
-        .then(data => {
+        .then((response) => response.json())
+        .then((data) => {
             if (data.success == false) {
                 $("#delete-error").html(`<p class="link-danger">Password does not match.</p>`)
             } else {
                 location.replace("/")
             }
         })
-        .catch(error => {
-            console.error('Error:', error);
+        .catch((error) => {
+            console.error("Error:", error);
         });
 });
