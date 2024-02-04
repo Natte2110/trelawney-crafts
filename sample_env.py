940dfd730ee1fd26c8cab9dc858c5ed8a6292702
Natte2110 <natteparsley@gmail.com>
2024-02-04 21:37:44 +0000

    feat: Add sample env variables

---
+++ sample_env.py
@@ -0,0 +1,9 @@
+import os
+
+os.environ.setdefault("IP", "0.0.0.0")
+os.environ.setdefault("PORT", "80")
+os.environ.setdefault("SECRET_KEY", "YOUR_SECRET_KEY")
+os.environ.setdefault("DEBUG", "False")
+os.environ.setdefault("DEVELOPMENT", "False")
+os.environ.setdefault("DB_URL", "YOUR_DB_URL")
+os.environ.setdefault("UPLOAD_FOLDER", "trelawneycrafts/static/media/user_uploads")
