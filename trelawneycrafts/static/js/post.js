eab203e1181ddd960979f8c36baf7c26b6be80cc
Natte2110 <natteparsley@gmail.com>
2024-02-06 21:51:55 +0000

    refactor: Add header comment to explain file use

--- trelawneycrafts/static/js/post.js
+++ trelawneycrafts/static/js/post.js
@@ -1,3 +1,8 @@
+/**
+* @file This script is used in order to create a preview
+* image on the "Create Post"/"upload" page
+* @author Nathan Parsley
+*/
 let imageInput = document.getElementById("image");
 let imagePreview = document.getElementById("image-preview");
 imageInput.onchange = function () {
